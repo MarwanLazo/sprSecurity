4b8b4dc3138af79c37f71930193dafb7dadc0395
Marwan <eng.marawan1988@yahoo.com>
2016-05-08 16:00:02 +0200

    servlet with spring integration success

--- sprSecurity/src/main/java/com/sprSecurity/spring/web/servlet/HelloServlet.java
+++ sprSecurity/src/main/java/com/sprSecurity/spring/web/servlet/HelloServlet.java
@@ -3,16 +3,15 @@ package com.sprSecurity.spring.web.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.faces.context.FacesContext;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.jsf.FacesContextUtils;
+import org.apache.log4j.Logger;
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
 import com.sprSecurity.spring.data.service.TempTableService;
 import com.sprSecurity.spring.dto.TempTableDTO;
@@ -23,36 +22,45 @@ import com.sprSecurity.spring.enums.Status;
  */
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	@Autowired
-	private TempTableService  service;
-	
+	private static final long	serialVersionUID	= 1L;
+	@Inject
+	private TempTableService	service;
+
+	private Logger				logger				= Logger.getLogger(HelloServlet.class);
+
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
 	public HelloServlet() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		logger.info("intgerate Spring context with servlet context");
+		SpringBeanAutowiringSupport.processInjectionBasedOnServletContext(this, this.getServletContext());
+		logger.info("intgerate Done !! Successfully");
+	}
+
 	/**
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void service (HttpServletRequest request , HttpServletResponse response) throws ServletException , IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter out = response.getWriter();
 		try {
 			TempTableDTO dto = new TempTableDTO();
 			dto.setStatus(Status.ACTIVE);
-			dto.setId("Ali");
-			dto.setTempEmail("ali@gmail.com");
-			ApplicationContext ctx = FacesContextUtils.getWebApplicationContext(FacesContext.getCurrentInstance());
-			service = ctx.getBean(TempTableService.class);
-			service.createEntity(dto);
-			out.println("");
+			dto.setId("Adam");
+			dto.setTempEmail("adam@gmail.com");
+			logger.info("sevlet method 'Sarvice' create or update  temp table");
+			dto = service.createEntity(dto);
+			logger.info("creation Done !! successfully	");
+			out.println(dto);
 		} finally {
 			out.close();
 		}
 	}
-	
+
 }
