e22f117ee7954763d6790b6ef6f1d0fada6ebbfd
Marwan <Marwan@Marwan-PC>
2016-05-14 09:39:03 +0200

    remove spring batch

--- sprSecurity/src/main/java/com/sprSecurity/spring/dto/TempTableDTO.java
+++ sprSecurity/src/main/java/com/sprSecurity/spring/dto/TempTableDTO.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sprSecurity.spring.hibernate.entity.TempTableEB;
 import com.sprSecurity.spring.oval.UpperCase;
 import com.sprSecurity.spring.oval.ValidID;
@@ -12,77 +16,86 @@ import net.sf.oval.constraint.MatchPattern;
 import net.sf.oval.constraint.NotBlank;
 
 public class TempTableDTO extends AbstractDTO<String> {
-	
-	private static final long	serialVersionUID = 1L;
-	private static final String	EMAIL_PATTERN	 = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
-	
+
+	private static final long	serialVersionUID	= 1L;
+	private static final String	EMAIL_PATTERN		= "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+
 	@NotBlank
 	@NotNull(message = "Name cannot be null")
 	@UpperCase
 	@ValidID
 	private String				tempTableName;
-	
+
 	@NotNull
 	@MatchPattern(pattern = EMAIL_PATTERN, message = "invalid email Oval")
 	private String				tempEmail;
-	
+
+	@JsonInclude(Include.NON_NULL)
 	private TempTableDTO		tempRef;
+
 	
 	private Date				createTime;
-	
+
 	public TempTableDTO() {
 	}
-	
+
 	public TempTableDTO(String tempName, String tempEmail) {
 		super();
 		this.tempTableName = tempName;
 		this.tempEmail = tempEmail;
 	}
-	
-	
-	public String getTempEmail () {
+
+	public String getTempEmail() {
 		return tempEmail;
 	}
-	
-	public void setTempEmail (String tempEmail) {
+
+	public void setTempEmail(String tempEmail) {
 		this.tempEmail = tempEmail;
 	}
-	
-	public TempTableDTO getTempRef () {
+
+	public TempTableDTO getTempRef() {
 		return tempRef;
 	}
-	
-	public void setTempRef (TempTableDTO tempRef) {
+
+	public void setTempRef(TempTableDTO tempRef) {
 		this.tempRef = tempRef;
 	}
-	
+
 	@Override
-	public String toString () {
+	public String toString() {
 		return "[" + tempTableName + ":" + tempEmail + "]";
 	}
-	
+
 	@Override
-	public boolean equals (Object obj) {
-		if (obj == null) return false;
+	public boolean equals(Object obj) {
+		if (obj == null)
+			return false;
 		if (obj instanceof TempTableDTO) {
 			TempTableDTO temp = (TempTableDTO) obj;
-			if (temp.tempTableName != null && temp.tempEmail != null && temp.tempEmail.equals(this.tempEmail) && temp.tempTableName.equals(this.tempTableName)) return true;
+			if (temp.tempTableName != null && temp.tempEmail != null && temp.tempEmail.equals(this.tempEmail) && temp.tempTableName.equals(this.tempTableName))
+				return true;
 		}
 		return false;
 	}
-	
+
+	@JsonIgnore
+	@JsonProperty(value = "pK")
 	@Override
-	public String getPK () {
+	public String getPK() {
 		return getTempTableName();
 	}
-	
+
+	@JsonIgnore
+	@JsonProperty(value = "pKAsString")
 	@Override
-	public String getPKAsString () {
+	public String getPKAsString() {
 		return getTempTableName().toString();
 	}
-	
+
+	@JsonIgnore
+	@JsonProperty(value = "entityClass")
 	@Override
-	public Class<TempTableEB> getEntityClass () {
+	public Class<TempTableEB> getEntityClass() {
 		return TempTableEB.class;
 	}
 
@@ -94,6 +107,8 @@ public class TempTableDTO extends AbstractDTO<String> {
 		this.tempTableName = tempTableName;
 	}
 
+	@JsonIgnore
+	@JsonProperty(value = "createTime")
 	public Date getCreateTime() {
 		return createTime;
 	}
@@ -101,5 +116,5 @@ public class TempTableDTO extends AbstractDTO<String> {
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
-	
+
 }
