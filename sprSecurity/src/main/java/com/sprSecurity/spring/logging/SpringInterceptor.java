f55286aae79dcaaed10f539231783070428e365f
Marwan <eng.marawan1988@yahoo.com>
2016-07-25 11:18:24 +0200

    fix logging

--- sprSecurity/src/main/java/com/sprSecurity/spring/logging/SpringInterceptor.java
+++ sprSecurity/src/main/java/com/sprSecurity/spring/logging/SpringInterceptor.java
@@ -3,35 +3,32 @@ package com.sprSecurity.spring.logging;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class SpringInterceptor extends HandlerInterceptorAdapter {
 
-	// private static final Logger logger = LoggerFactory
-	// .getLogger(RequestProcessingTimeInterceptor.class);
+	private Logger logger = Logger.getLogger(SpringInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		long startTime = System.currentTimeMillis();
-		System.out.println("Request URL::" + request.getRequestURL().toString() + ":: Start Time=" + System.currentTimeMillis());
+		logger.info("Request URL::" + request.getRequestURL().toString() + ":: Start Time=" + System.currentTimeMillis());
 		request.setAttribute("startTime", startTime);
-		// if returned false, we need to make sure 'response' is sent
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		System.out.println("Request URL::" + request.getRequestURL().toString() + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
-		// we can add attributes in the modelAndView and use that in the view
-		// page
+		logger.info("Request URL::" + request.getRequestURL().toString() + " Sent to Handler :: Current Time=" + System.currentTimeMillis());
 	}
 
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
 		long startTime = (Long) request.getAttribute("startTime");
-		System.out.println("Request URL::" + request.getRequestURL().toString() + ":: End Time=" + System.currentTimeMillis());
-		System.out.println("Request URL::" + request.getRequestURL().toString() + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
+		logger.info("Request URL::" + request.getRequestURL().toString() + ":: End Time=" + System.currentTimeMillis());
+		logger.info("Request URL::" + request.getRequestURL().toString() + ":: Time Taken=" + (System.currentTimeMillis() - startTime));
 	}
 
 	@Override
